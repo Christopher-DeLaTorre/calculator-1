80d91fd48cb1132eb411e122bb0331b0154908e9
Christopher-DeLaTorre <delatorrechris18@gmail.com>
2020-09-08 12:03:13 -0700

    Initial commit

---
+++ custom calc.py
@@ -0,0 +1,4 @@
+num1 = int(input("input a number"))#input asked for first variable and converted to int
+num2 = int(input("input another number"))#^^
+result = num1 * num2#simple calculation stored in new variable
+print(result)#print calculation
