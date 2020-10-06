d3eb3a5959703289f25cdafb112b64f892178885
Christopher-DeLaTorre <69987322+Christopher-DeLaTorre@users.noreply.github.com>
2020-10-06 10:16:45 -0700

    Add files via upload

--- custom calc.py
+++ custom calc.py
@@ -1,4 +1,6 @@
-num1 = int(input("input a number"))#input asked for first variable and converted to int
-num2 = int(input("input another number"))#^^
-result = num1 * num2#simple calculation stored in new variable
+num1 = int(input("input a number "))#input asked for first variable and converted to int
+num2 = int(input("input another number "))#^^
+num3 = int(input("input one last number "))#^
+result = num1 + num2 + num3 #simple calculation stored in new variable adds all together
+result = result / 3 #gets the average of the three numbers
 print(result)#print calculation
